0f2d79fb1e5354176de0cdec710ced32b541f6ae
Edgar Mijares <contacto@ecmc.me>
2020-03-26 23:53:26 -0600

    Category label

--- src/app/landing/categorias/categorias.component.ts
+++ src/app/landing/categorias/categorias.component.ts
@@ -9,12 +9,23 @@ import { RouterLinkActive, ActivatedRoute } from '@angular/router';
 export class CategoriasComponent implements OnInit {
 
     name = '';
+    selection: string = 'todos';
+    categoria: any = {
+        name: 'Arquitectura',
+        decription: '',
+        image: 'https://firebasestorage.googleapis.com/v0/b/apitecto-d77cd.appspot.com/o/category%2Ficon%2Farquitectura.png?alt=media&token=a3b4cc9e-344d-463f-9868-c8e861a46402',
+        cover: './assets/img/bg/arquitectura.jpg',
+        path: ['/categoria', 'arquitectura']
+    };
 
     constructor(private router: ActivatedRoute) {
         this.name = this.router.snapshot.paramMap.get('name_category');
     }
 
-    ngOnInit(): void {
+    ngOnInit(): void {}
+
+    selectionList(selection: string) {
+        this.selection = selection;
     }
 
 }
